8110250f52eef84a105f9f9300986f9c701292eb
Elyoni003 <117513798+Elyoni003@users.noreply.github.com>
2022-11-14 20:12:04 +0100

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ API_ID =  os.environ.get('api_id','15158057')
 API_HASH = os.environ.get('api_hash','6a7956a4a75bd2ed46a021649e4c9edf')
 SPLIT_FILE = 1024 * 1024 * int(os.environ.get('split_file','99'))
 ROOT_PATH = 'root/'
-ACCES_USERS = os.environ.get('tl_admin_user','potterhead5').split(';')
+ACCES_USERS = os.environ.get('YosmelGarcia','potterhead5').split(';')
 PROXY = ProxyCloud.parse(os.environ.get('proxy_enc','http://KFGGJJYGJKLHFKYEDIGDYJJEKEFKREEGLFDKLH'))
 
 if PROXY:
