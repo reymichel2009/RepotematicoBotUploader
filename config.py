507b31716b76e5ab4b8cb1d65f547dfa94a44721
the joker <117513798+Elyoni003@users.noreply.github.com>
2022-11-22 01:06:24 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 import os
 import ProxyCloud
 
-BOT_TOKEN =  os.environ.get('bot_token','5710756534:AAFJqdgFCg6i4OEZYtkkroI_A4rMGiKonlQ')
+BOT_TOKEN =  os.environ.get('bot_token','5710756534:AAE1ihDreMkihVYNTZ4_XdhXz5cpFXItPps')
 API_ID =  os.environ.get('api_id','15158057')
 API_HASH = os.environ.get('api_hash','6a7956a4a75bd2ed46a021649e4c9edf')
 SPLIT_FILE = 1024 * 1024 * int(os.environ.get('split_file','99'))
 ROOT_PATH = 'root/'
-ACCES_USERS = os.environ.get('LevAndrade93','LevAndrade93').split(';')
+ACCES_USERS = os.environ.get('LevAndrade93','admin3').split(';')
 PROXY = ProxyCloud.parse(os.environ.get('proxy_enc','http://KFGGJJYGJKLHFKYEDIGDYJJEKEFKREEGLFDKLH'))
 
 if PROXY:
