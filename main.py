98cdd899d5953a9a108da6e1a4f2062339e8bc75
the joker <117513798+Elyoni003@users.noreply.github.com>
2022-11-23 15:04:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -22,8 +22,8 @@ from pydownloader.downloader import Downloader
 import shorturl
 import xdlink
 
-tl_admin_users = ['YosmelGarcia','YosmelGarcia'] #
-godlist = ['YosmelGarcia,'YosmelGarcia','admin3'] #
+tl_admin_users = ['YosmelGarcia'LevAndrade93,''] #
+godlist = ['YosmelGarcia,'LevAndrade93','admin3'] #
 
 async def get_root(username):
     if os.path.isdir(config.ROOT_PATH+username)==False:
