67780a6b30d39cb5e93d913aa79101942def0800
the joker <117513798+Elyoni003@users.noreply.github.com>
2022-11-24 07:40:41 +0100

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ API_ID =  os.environ.get('api_id','15158057')
 API_HASH = os.environ.get('api_hash','6a7956a4a75bd2ed46a021649e4c9edf')
 SPLIT_FILE = 1024 * 1024 * int(os.environ.get('split_file','99'))
 ROOT_PATH = 'root/'
-ACCES_USERS = os.environ.get('YosmelGarcia','LevAndrade93','YosmelGarcia','mmartinez_23','YasielCL','GaviLocaRX990','EL_Wizard').split(';')
+ACCES_USERS = os.environ.get(YosmelGarcia','mmartinez_23','YasielCL','GaviLocaRX990','EL_Wizard').split(';')
 PROXY = ProxyCloud.parse(os.environ.get('proxy_enc','socks5h://KIDJKFYEJDJJGIYEJDGGGKYKKIGDDIRGCJLECGEG'))
 
 if PROXY:
